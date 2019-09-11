db63291cbac62a9aa8309537caa007c45494b4d9
ruchi bhindwale <rbhindwale@ruchis-MacBook-Pro.local>
2019-09-11 19:59:57 +0530

    Added code to handle add action

--- Node/index.js
+++ Node/index.js
@@ -3,15 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = process.env.port || 5555;
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const fs = require('fs');
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/books', function(req, res){
     fs.readFile('./data/books.json', 'utf8', (err, data) => {
-        let books = JSON.parse(data)
-        res.json(books);
+        res.json(JSON.parse(data));
     })
 })
 
@@ -20,13 +20,23 @@ app.get('/book/:id', function(req, res){
     res.send('<html><head><link href=assets/main.css type=text/css rel=stylesheet/></head><body><h1> Person '+ req.params.id +'</h1></body></html>');
 })
 
-
 // Reading form body and posting it 
-app.post('/book', urlencodedParser, function(req, res){
-    res.send('Thank you for form data');
-    console.log('First Name', req.body.firstName);
-    console.log('Last Name', req.body.lastName);
+app.post('/book', function(req, res){
+    fs.readFile('./data/books.json', 'utf-8', function(err, data) {
+        if (err) throw err
+    
+        let bookObjects = JSON.parse(data);
+        console.log('bookObjects', bookObjects);
+        bookObjects.push(req.body);
+        console.log('-------------------------------------------')
+        console.log('bookObjects after push', bookObjects);
+        
+        fs.writeFile('./data/books.json', JSON.stringify(bookObjects), 'utf-8', function(err) {
+            if (err) throw err
+            res.status(200);
+        })
+    })
 })
 
-app.listen(3003);
+app.listen(3007);
 
