8ce5d90c8c5de2bac963aeaeacaa43f4fc9c4095
ruchi bhindwale <rbhindwale@ruchis-MacBook-Pro.local>
2019-09-12 12:48:41 +0530

    Added methods for edit

--- Node/index.js
+++ Node/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = process.env.port || 5555;
+const port = process.env.port || 3007;
 const fs = require('fs');
 
 app.use(cors());
@@ -17,26 +17,57 @@ app.get('/books', function(req, res){
 
 // PATTERN URL - person/:id example 
 app.get('/book/:id', function(req, res){
-    res.send('<html><head><link href=assets/main.css type=text/css rel=stylesheet/></head><body><h1> Person '+ req.params.id +'</h1></body></html>');
+    fs.readFile('./data/books.json', 'utf8', (err, data) => {
+        const books = JSON.parse(data);
+        const selectedBook = books.filter(book => req.params.id == book.id);
+        res.json(selectedBook);
+    })
 })
 
-// Reading form body and posting it 
-app.post('/book', function(req, res){
+app.post('/add', function(req, res){
     fs.readFile('./data/books.json', 'utf-8', function(err, data) {
         if (err) throw err
     
         let bookObjects = JSON.parse(data);
-        console.log('bookObjects', bookObjects);
         bookObjects.push(req.body);
-        console.log('-------------------------------------------')
-        console.log('bookObjects after push', bookObjects);
         
         fs.writeFile('./data/books.json', JSON.stringify(bookObjects), 'utf-8', function(err) {
             if (err) throw err
-            res.status(200);
+            res.status(201).json({
+                status: 'success',
+                message: 'Book successfully added'
+              });
         })
     })
 })
 
-app.listen(3007);
+app.post('/edit', function(req, res){
+    fs.readFile('./data/books.json', 'utf-8', function(err, data) {
+        if (err) throw err
+    
+        let bookObjects = JSON.parse(data);
+
+        for (let book of bookObjects){
+            if(book.id == req.body.id){
+                let updatedBookData = req.body;
+                book.name = updatedBookData.name;
+                book.author = updatedBookData.author;
+                book.description = updatedBookData.description;
+                book.count = updatedBookData.count;
+                book.publishDate = updatedBookData.publishDate;
+                book.genre = updatedBookData.genre;
+            }
+        }
+        
+        fs.writeFile('./data/books.json', JSON.stringify(bookObjects), 'utf-8', function(err) {
+            if (err) throw err
+            res.status(201).json({
+                status: 'success',
+                message: 'Book successfully edited'
+              });
+        })
+    })
+})
+
+app.listen(port);
 
